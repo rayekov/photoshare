c223c30bca826119f7884da3cca942bea3ec033f
rayekov <hervefraye@gmail.com>
2023-05-02 22:32:07 +0100

    Learned about pipes
    format dates, numbers, percentage, currencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedPhotoComponent } from './shared-photo/shared-photo.component';
+import { registerLocaleData } from '@angular/common';
+import * as en from '@angular/common/locales/en';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,13 @@ import { SharedPhotoComponent } from './shared-photo/shared-photo.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-Us'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(en.default);
+  }
+}
