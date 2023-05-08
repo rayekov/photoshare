a7f5252e095730a617eca248ba94be2959d32b09
rayekov <hervefraye@gmail.com>
2023-05-08 02:23:45 +0100

    learned about Observables
    observables ,low level operators(map,filter,tap)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+  interval$!:Observable<string>
   
   ngOnInit(){
+    this.interval$ = interval(1000).pipe(
+      filter(value => value %3 ===0),
+      map(value => value%2===0? 
+        `Je suis ${value} et je suis pair`: 
+        `je suis ${value} et je suis impair`
+        ),
+        tap(text => this.logger(text))
+    );
     
+
+  }
+
+  logger(text :  string){
+    console.log(`log: ${text}`)
   }
 
 }
